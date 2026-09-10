090b692a7c32c4603b8e9f5bd96701beb390b9c6
Bautista Reina <breina@frba.utn.edu.ar>
2026-09-10 11:57:22 -0300

    Create cierre.cpp

---
+++ datos/cierre.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+#include <cstring>
+#include <cstdio> 
+using namespace std; 
+
+struct comanda
+{
+    char fecha[11]; 
+    char mozo[53]; 
+    int codprod;
+    int cantidad; 
+    int comision; 
+};
+
+
+
+
+int main(){
+
+FILE* archivo = fopen ("C:/Users/reina_bjncudd/OneDrive/Documentos/GitHub/tp-N-4-2026/datos/comandas_historicas.dat", "rb");
+if (archivo == NULL)
+{
+    cout << "No es posible abrir el archivo" << endl; 
+    return 0; 
+}
+
+comanda comandas[1000]; 
+int cantidad = 0; 
+while (fread (&comandas [cantidad], sizeof (comanda),1 , archivo) ==1)
+{
+    cantidad++;
+}
+fclose(archivo); 
+
+
+
+
+}
