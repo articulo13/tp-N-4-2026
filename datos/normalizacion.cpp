6a9309e8112e54f08711b000e36008f25a33e57e
Alexander Ezequiel Catari Alarcon <acatarialarcon@frba.utn.edu.ar>
2026-09-15 22:21:27 -0300

    cambios normalizacion

--- datos/normalizacion.cpp
+++ datos/normalizacion.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<cstdio>
+
+using namespace std;
+
 struct Producto {    // estructura del stock 
 	int   codigo;   
 	char descripcion[50];   
@@ -7,12 +10,30 @@ struct Producto {    // estructura del stock
 	int stockActual; 
 };
 
-using namespace std;
 
+struct mozo{
+	
+	int id;
+	char nombre[50];
+	char password[50];  
+	float TotalComision;
+	
+	
+};
 
-
+struct ComandaHistorica{               //struct del archivo binario con el mismo nombre
+	
+	char fecha[11];
+	char nombreMozo[53];
+	int codProd;  //codigo del producto
+	int cantidad;
+	float comision;
+};
 
 int main(){
+	
+	
+	
 	
 	
 	
